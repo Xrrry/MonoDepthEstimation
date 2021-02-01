265cf35f59cfa6a535f3e7ed3d716b1da6ad63a2
Xrrry <48988621+Xrrry@users.noreply.github.com>
2021-02-01 15:13:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/monodepthestimation/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.monodepthestimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        // 设置状态栏字体颜色 黑色
+        Window window = getWindow();
+        if (window != null) {
+            Class clazz = window.getClass();
+            try {
+                int darkModeFlag = 0;
+                Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+                Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+                darkModeFlag = field.getInt(layoutParams);
+                Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+                extraFlagField.invoke(window, darkModeFlag, darkModeFlag);//状态栏透明且黑色字体
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //开发版 7.7.13 及以后版本采用了系统API，旧方法无效但不会报错，所以两个方式都要加上
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
+            } catch (Exception e) {
+
+            }
+        }
+        setContentView(R.layout.activity_main);
+    }
+}
